65c0589a0beb3d4be5396630da94c7d7f650805c
Tarik Boumehdi <tboumehdi@alca-calgary.com>
2026-01-21 14:54:47 -0700

    adjust polygones

--- components/map/MapView.tsx
+++ components/map/MapView.tsx
@@ -5,8 +5,6 @@ import "mapbox-gl/dist/mapbox-gl.css"
 import { useState } from "react"
 import organizations from "@/lib/org.json"
 import { MapPin } from 'lucide-react'
-
-// ✅ Import your GeoJSON file
 import francophoneRegions from "@/lib/francophone-regions.json"
 
 // ✅ Layer styles
@@ -14,7 +12,7 @@ const regionFill: LayerProps = {
   id: "regions-fill",
   type: "fill",
   paint: {
-    "fill-color": "#16b1f9", // orange
+    "fill-color": "#16b1f9", 
     "fill-opacity": 0.2
   }
 }
@@ -43,7 +41,11 @@ const regionLabels: LayerProps = {
   }
 }
 
+
+
 export function MapView() {
+  const [mapLoaded, setMapLoaded] = useState(false)
+
   const [viewState, setViewState] = useState({
     longitude: -113.4711,
     latitude: 53.5198,
@@ -59,15 +61,22 @@ export function MapView() {
         mapStyle="mapbox://styles/bmhdi/cmkoaod33000k01r83dx855i3"
         style={{ width: "100%", height: "100%" }}
         onMove={(e) => setViewState(e.viewState)}
+         onLoad={() => setMapLoaded(true)}
       >
         <NavigationControl position="top-right" />
 
         {/* ✅ Add the Francophone regions */}
-        <Source id="francophone-regions" type="geojson" data={{ type: "FeatureCollection", features: francophoneRegions.features as any[] }}>
-          <Layer {...regionFill} />
-          <Layer {...regionBorder} />
-          <Layer {...regionLabels} />
-        </Source>
+       {mapLoaded && (
+  <Source
+    id="francophone-regions"
+    type="geojson"
+    data={francophoneRegions as any}
+  >
+    <Layer {...regionFill} />
+    <Layer {...regionBorder} />
+    <Layer {...regionLabels} />
+  </Source>
+)}
 
         {/* ✅ MARKERS */}
         {organizations.map((org) => (
