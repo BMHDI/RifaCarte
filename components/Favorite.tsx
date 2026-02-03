0e4e91396c98b0e4b428a8cac940c9addc95032e
Tarik <tarikboumehdi91@gmail.com>
2026-02-02 21:17:35 -0700

    forced SAVEDWORK

--- components/Favorite.tsx
+++ components/Favorite.tsx
@@ -18,7 +18,7 @@ export default function FavoritesPage() {
         <div className=" grid gap-4 overflow-y-auto [grid-template-columns:repeat(auto-fit,minmax(280px,1fr))]">
       {savedOrgs.map((org) => (
         <OrgCard
-          logo=""
+          image_url={org.image_url ?? ""}
           key={org.id}
           name={org.name}
           phone={org.contact?.phone ?? ""}
