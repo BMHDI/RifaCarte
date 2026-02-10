91c1125014c70f63d5bf9741965734f312641a49
Tarik Boumehdi <tboumehdi@alca-calgary.com>
2026-02-10 11:48:45 -0700

    Updated version based on Client preferences

--- lib/encoder.ts
+++ lib/encoder.ts
@@ -5,14 +5,14 @@ export async function GET(req: Request) {
   const stream = new ReadableStream({
     start(controller) {
       // We will push data here later
-    }
+    },
   });
 
   return new Response(stream, {
     headers: {
       'Content-Type': 'text/event-stream',
       'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive',
+      Connection: 'keep-alive',
     },
   });
 }
