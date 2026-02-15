91ae84779875d01fd768c9a89a6f158d900e43ea
Tarik <tarikboumehdi91@gmail.com>
2026-02-14 17:08:34 -0700

    lazy load for map Lighthouse performance

--- components/map/lazyMap.tsx
+++ components/map/lazyMap.tsx
@@ -2,12 +2,13 @@
 
 import dynamic from 'next/dynamic';
 import { Spinner } from '../ui/spinner';
+import MapSkeleton from './mapSkelton';
 
 const MapView = dynamic(() => import('./MapView').then(m => m.MapView), {
   ssr: false,
   loading: () => (
     <div className="flex h-screen items-center justify-center">
-      <Spinner className="h-12 w-12" />
+     <MapSkeleton/>
     </div>
   ),
 });
---
+++ components/map/mapSkelton.tsx
@@ -0,0 +1,7 @@
+export default function MapSkeleton() {
+  return (
+    <div className="w-screen h-screen bg-gray-200 animate-pulse flex items-center justify-center">
+      <p className="text-gray-500 text-sm">Loading map…</p>
+    </div>
+  );
+}
